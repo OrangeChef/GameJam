65cc2838fb7b4d72abb23cd65ca11370410e5edb
OrangeChef <redyellowchef@gmail.com>
2021-10-23 10:51:00 -0400

    Added pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    [Header("Action on Key Press")]
+    public ToggleMenuOnKeyPress[] menuMappings;
+    public LoadSceneOnKeyPress[] sceneMappings;
+
+    void Update()
+    {
+        if (menuMappings.Length > 0)
+        {
+            foreach (ToggleMenuOnKeyPress tmokp in menuMappings)
+            {
+                if (Input.GetKeyDown(tmokp.key))
+                {
+                    if (tmokp.menu.isMenuOpen)
+                        MenuManager.Instance.CloseMenu(tmokp.menu);
+                    else
+                        MenuManager.Instance.OpenMenu(tmokp.menu);
+                }
+            }
+        }
+
+        if (sceneMappings.Length > 0)
+        {
+            foreach (LoadSceneOnKeyPress lsokp in sceneMappings)
+            {
+                if (Input.GetKeyDown(lsokp.keyToLoad))
+                    LoadScene(lsokp.sceneName);
+            }
+        }
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
@@ -25,4 +56,18 @@ public class GameManager : MonoBehaviour
 
         Application.Quit(exitCode);
     }
+}
+
+[System.Serializable]
+public class ToggleMenuOnKeyPress
+{
+    public Menu menu;
+    public KeyCode key;
+}
+
+[System.Serializable]
+public class LoadSceneOnKeyPress
+{
+    public string sceneName;
+    public KeyCode keyToLoad;
 }
